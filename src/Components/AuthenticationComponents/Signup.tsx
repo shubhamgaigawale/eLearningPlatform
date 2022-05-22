1d4ef7ba04bbb1f9161a71652e097113e1795cb2
Dheeraj Johare <dhirajjohare1497@gmail.com>
2022-05-22 17:45:51 +0530

    WIP : Login and Signup Forms

--- src/Components/AuthenticationComponents/Signup.tsx
+++ src/Components/AuthenticationComponents/Signup.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import { TextField } from '@mui/material'
-// import {DatePicker} from '@mui/material'
-export default function Signup() {
-  return (
-    <div>hello from Signup page
-        <div className=''>
-            <TextField id="outlined-basic" label="First name" variant="outlined" />
-            <TextField id="outlined-basic" label="last Name" variant="outlined" />
-            <TextField id="outlined-basic" label="Email id" variant="outlined" />
-            <TextField id="outlined-basic" label="Password" variant="outlined" />
-            {/* <LocalizationProvieder dateAdapter={AdapterDateFns}>
-                <DatePicker label="Birth Date"/>
+import { Button, TextField } from '@mui/material'
+import './Signup.less'
 
-            </LocalizationProvieder> */}
+
+export default function Signup() {
+    return (
+        <div>hello from Signup page
+            <div className='Signup'>
+                <TextField id="outlined-basic" label="First name" variant="outlined" />
+                <TextField id="outlined-basic" label="last Name" variant="outlined" />
+                <TextField id="outlined-basic" label="Email id" variant="outlined" />
+                <TextField id="outlined-basic" label="Password" variant="outlined" />
+                <TextField type="date" />
+                <Button variant="contained">Signup </Button>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
