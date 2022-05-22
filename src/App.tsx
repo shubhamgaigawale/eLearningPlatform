41cdbfee71063609e02e444c2c6bc6b254a2abf7
Shubham Gaigawale <gaigawaleshubham@gmail.com>
2022-05-22 18:47:58 +0530

    Install React Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import "./App.css";
-import "./Components/UI Components/Common Components/HeaderComponents/Header"
+import "./Components/UI Components/Common Components/HeaderComponents/Header";
 import Header from "./Components/UI Components/Common Components/HeaderComponents/Header";
 import Signin from "./Components/AuthenticationComponents/Signin";
 import Signup from "./Components/AuthenticationComponents/Signup";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-     {/* <Header/> */}
+      {/* <Header/> */}
       {/* <h1>Hello World</h1> */}
-      <Signup/>
-      <Signin/>
+      {/* <Signup />
+      <Signin /> */}
+
+      <Routes>
+        <Route path="signup" element={<Signup />} />
+        <Route path="signin" element={<Signin />} />
+      </Routes>
     </div>
   );
 }
--- src/Components/AuthenticationComponents/Signup.tsx
+++ src/Components/AuthenticationComponents/Signup.tsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import { TextField } from '@mui/material'
+import React from "react";
+import { TextField } from "@mui/material";
 // import {DatePicker} from '@mui/material'
 export default function Signup() {
   return (
-    <div>hello from Signup page
-        <div className=''>
-            <TextField id="outlined-basic" label="First name" variant="outlined" />
-            <TextField id="outlined-basic" label="last Name" variant="outlined" />
-            <TextField id="outlined-basic" label="Email id" variant="outlined" />
-            <TextField id="outlined-basic" label="Password" variant="outlined" />
-            {/* <LocalizationProvieder dateAdapter={AdapterDateFns}>
+    <div>
+      hello from Signup page
+      <div className="">
+        <TextField id="outlined-basic" label="First name" variant="outlined" />
+        <TextField id="outlined-basic" label="last Name" variant="outlined" />
+        <TextField id="outlined-basic" label="Email id" variant="outlined" />
+        <TextField id="outlined-basic" label="Password" variant="outlined" />
+        {/* <LocalizationProvieder dateAdapter={AdapterDateFns}>
                 <DatePicker label="Birth Date"/>
 
             </LocalizationProvieder> */}
-        </div>
+      </div>
     </div>
-  )
+  );
 }
